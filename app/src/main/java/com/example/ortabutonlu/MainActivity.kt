71bde79d9db8c1dbba0983dd41db9802710e67b8
burak <design.burakk@gmail.com>
2022-06-29 14:16:13 +0300

    some changes

--- app/src/main/java/com/example/ortabutonlu/MainActivity.kt
+++ app/src/main/java/com/example/ortabutonlu/MainActivity.kt
@@ -2,9 +2,19 @@ package com.example.ortabutonlu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.ortabutonlu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +24,11 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavMenu.background = null
 
+        navController = findNavController(R.id.fragmentContainerView)
+        setupWithNavController(binding.bottomNavMenu,navController)
+
+        binding.floatbutton.setOnClickListener {
+            Toast.makeText(it!!.context, "Added in basket", Toast.LENGTH_SHORT).show()
+        }
     }
 }
